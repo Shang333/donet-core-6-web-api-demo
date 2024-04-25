d86c4a7f7615606e1afc5916821a67c31b8c0041
TWTPEN1608033S2\USER <s881672000@gmail.com>
2024-04-25 11:11:13 +0800

    intstall nuget packages

--- WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c8e0bc363a51471e7253336534670b82e2bfc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2e3cad543f80cd61cc3d83e206176e411a1e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
