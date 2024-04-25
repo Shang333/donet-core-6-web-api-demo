52c8e0bc363a51471e7253336534670b82e2bfc5
TWTPEN1608033S2\USER <s881672000@gmail.com>
2024-04-25 10:56:29 +0800

    delete weather api  sample

--- WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f84335573c5ca23828568af56f91fdf273a5d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac38099c936f350b422fe852cb7d33ff4c86b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
